86e4b26f437c17238146b82f8e167fc867dcb026
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2026-01-20 22:45:05 +0100

    feat: create admin route and save to db

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,17 +1,27 @@
 from typing import Annotated
 from fastapi import APIRouter, Form
-from models import AdminLogin, AdminSignup
+from models.admin import AdminLogin, AdminSignup, Admin
+from db.database import SessionDep
+
 
 router = APIRouter()
 
 
 @router.post("/admin/login")
-async def login(form: Annotated[AdminLogin, Form()]):
+async def login(form: Annotated[AdminLogin, Form()], session: SessionDep):
     email, password = form.email, form.password
 
     return {"message": "Login endpoint"}
 
 
 @router.post("/admin/signup")
-async def signup(form: Annotated[AdminSignup, Form()]):
-    return {"message": "Signup endpoint"}
+async def signup(form: Annotated[AdminSignup, Form()], session: SessionDep):
+    admin = Admin.model_validate(form.model_dump())
+    session.add(admin)
+    session.commit()
+    session.refresh(admin)
+
+    return {
+        "name": admin.name,
+        "email": admin.email,
+    }
--- backend/models/admin.py
+++ backend/models/admin.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr, field_validator
+from sqlmodel import SQLModel, Field
 
 
 def validate_password_strength(v: str) -> str:
@@ -15,19 +16,26 @@ def validate_password_strength(v: str) -> str:
     return v
 
 
-class AdminLogin(BaseModel):
+class AdminBase(SQLModel):
     email: EmailStr
     password: str
 
+
+class Admin(AdminBase, table=True):
+    __tablename__ = "admin"  # type: ignore
+
+    id: int = Field(default=None, primary_key=True)
+    name: str
+
+
+class AdminLogin(AdminBase):
     @field_validator("password")
     def validate_password(cls, v: str):
         return validate_password_strength(v)
 
 
-class AdminSignup(BaseModel):
+class AdminSignup(AdminBase):
     name: str
-    email: EmailStr
-    password: str
 
     @field_validator("password")
     def validate_password(cls, v: str):
